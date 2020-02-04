5921922dcd0011b74846a610b2b15a9936926a0f
pyohei <mukaishohei76@gmail.com>
2020-02-04 16:57:34 +0900

    add server

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
+package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	root, _ := os.Getwd()
+	port := ":9999"
+	prefix := "/"
+	log.Print(root)
+
+	http.Handle(prefix, http.StripPrefix(prefix, http.FileServer(http.Dir(root))))
+
+	mux := http.DefaultServeMux.ServeHTTP
+	logger := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Print(r.RemoteAddr + " " + r.Method + " " + r.URL.String())
+		mux(w, r)
+	})
+
+	var err error
+	err = http.ListenAndServe(port, logger)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
